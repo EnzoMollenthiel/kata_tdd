a2dda3174e5239c6861f9ab74cc48a910c5a64f3
enzo <mollenthielenzo@gmail.com>
2020-03-06 11:53:31 +0100

    refactor

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,10 +3,8 @@ public class FizzBuzz {
     public String fizzBuzz(int n) {
         String result = "";
 
-        if(n % 3 == 0) result = "Fizz";
-        else if (n % 5 == 0) result = "Buzz";
-
-        if(n % 3 == 0 && n % 5 == 0) result = "FizzBuzz";
+        if (n % 3 == 0) result += "Fizz";
+        if (n % 5 == 0) result += "Buzz";
 
         return result;
     }
